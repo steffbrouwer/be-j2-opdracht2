adc27cbd440c5d9fc5b3cc858e6dff730c3d333e
Steff Brouwer <sbrouwer05@gmail.com>
2023-10-23 21:50:48 +0200

    config added

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Be-j2-opdracht2');
+define('URLROOT', 'http://www.be-j2-opdracht2.org');
